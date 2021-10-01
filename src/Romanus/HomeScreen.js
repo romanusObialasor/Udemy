52b6c9dcbf6124ac2dc331690123c23e279d4e4f
romanusObialasor <robialasor5@gmail.com>
2021-10-01 19:07:48 +0100

    common

--- src/Romanus/HomeScreen.js
+++ src/Romanus/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Main from "./components/Body/Main";
 import Land from "./components/Land";
 
 const HomeScreen = () => {
   return (
     <Container>
       <Land />
+      <Main />
     </Container>
   );
 };
---
+++ src/Romanus/components/Body/Main.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = () => {
+  return (
+    <Container>
+      <Wrapper></Wrapper>
+    </Container>
+  );
+};
+
+export default Main;
+
+const Container = styled.div``;
+
+const Wrapper = styled.div``;
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
+// const Container = styled.div``
